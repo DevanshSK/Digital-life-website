6a46bb45013615333cc78526f571e159749c03f8
DevanshSK <95367443+DevanshSK@users.noreply.github.com>
2022-02-05 21:42:23 +0530

    Adding  Recent Work Area -1

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 //* execute script after page load
 window.onload = function digital_fn() {
+  // Hamburger menu
   //? toggle button
   let toggle = document.querySelector("#nav .toggle-btn");
   let collapse = document.querySelector("#nav .collapse");
@@ -7,5 +8,17 @@ window.onload = function digital_fn() {
   // ? callback function
   toggle.addEventListener("click", function (event) {
     collapse.classList.toggle("active");
+  });
+
+  // Recent Work grid
+  // Mansonary JS
+  let grid = document.querySelector(
+    "#site-main .recent-work-area .images-flex"
+  );
+
+  let msnry = new Masonry(grid, {
+    itemSelector: ".flex-items",
+    gutter: 100,
+    fitWidth: true,
   });
 };
